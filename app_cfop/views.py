e77526ae80f46ea92ae7f10e976c8ec95661d7fc
Tales Palma <tales.palma_2022@outloo.com>
2024-03-25 20:24:49 -0300

    banco de dados configurando e listei as tabelas

--- app_cfop/views.py
+++ app_cfop/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-
+from .models import CfopEntrada, CfopCompra
 def home(request):
-    return render(request, 'index/home.html')
+    return render(request, 'home.html')
+
+def listarDados(request):
+    
+
+    cfopEntrada = CfopEntrada.objects.all()
+    cfopCompra = CfopCompra.objects.all()
+    dados = {'cfopEntrada': cfopEntrada, 'cfopCompra': cfopCompra}
+
+    return render(request, 'listarDados.html', dados)
--- project_cfop/urls.py
+++ project_cfop/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from app_cfop import views
 urlpatterns = [
     path('',views.home,name="home"),
+    path('listarDados/',views.listarDados,name="listarDados"),
 ]
+
