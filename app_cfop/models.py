0c3370bbbc79c24a30aa62de6cd8b710b95b8a73
Tales Palma <tales.palma_2022@outloo.com>
2024-03-25 20:06:22 -0300

    banco de dados configurado

--- app_cfop/models.py
+++ app_cfop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class CfopEntrada(models.Model):
+  cfop_com = models.CharField(max_length=4)
+  nome = models.CharField(max_length=70)
+  id_compra = models.ForeignKey('self', on_delete=models.CASCADE)
+
+
+class CfopCompra(models.Model):
+  id = models.AutoField(primary_key=True)
+  cfop_com = models.CharField(max_length=4, null=False)
+  nome = models.CharField(max_length=70)
